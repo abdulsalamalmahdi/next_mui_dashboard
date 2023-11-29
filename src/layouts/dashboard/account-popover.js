087535db32764b9f43df5c82e940cbe48eaa21c2
abdulsalamalmahdi <getmeips@gmail.com>
2023-11-29 22:32:38 +0100

    init commit

---
+++ src/layouts/dashboard/account-popover.js
@@ -0,0 +1,83 @@
+import { useCallback } from 'react';
+import { useRouter } from 'next/navigation';
+import PropTypes from 'prop-types';
+import { Box, Divider, MenuItem, MenuList, Popover, Typography} from '@mui/material';
+import {Dialog}  from '@mui/material';
+import  DialogProps  from '@mui/material/Dialog';
+import { styled } from '@mui/material/styles';
+import { useAuth } from 'src/hooks/use-auth';
+
+export const AccountPopover = (props) => {
+  const { anchorEl, onClose, open } = props;
+  const router = useRouter();
+  const auth = useAuth();
+
+  const handleSignOut = useCallback(
+    () => {
+      onClose?.();
+      auth.signOut();
+      router.push('/auth/login');
+    },
+    [onClose, auth, router]
+  );
+
+  // const BlurryDialog = styled(Dialog)<DialogProps>(({ theme }) => ({
+  //   backdropFilter: "blur(5px)",
+  //   // other styles here...
+  // }));
+  
+
+  return (
+    // <BlurryDialog>
+    <Popover
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        horizontal: 'left',
+        vertical: 'bottom'
+      }}
+      onClose={onClose}
+      open={open}
+      PaperProps={{ sx: { width: 200 } }}
+      BackdropProps={{ invisible: false }}
+    >
+      <Box
+        sx={{
+          py: 1.5,
+          px: 2
+        }}
+      >
+        <Typography variant="overline">
+          Account
+        </Typography>
+        <Typography
+          color="text.secondary"
+          variant="body2"
+        >
+         Cool Guy
+        </Typography>
+      </Box>
+      <Divider />
+      <MenuList
+        disablePadding
+        dense
+        sx={{
+          p: '8px',
+          '& > *': {
+            borderRadius: 1
+          }
+        }}
+      >
+        <MenuItem onClick={handleSignOut}>
+          Sign out
+        </MenuItem>
+      </MenuList>
+    </Popover>
+    // </BlurryDialog>
+  );
+};
+
+AccountPopover.propTypes = {
+  anchorEl: PropTypes.any,
+  onClose: PropTypes.func,
+  open: PropTypes.bool.isRequired
+};
