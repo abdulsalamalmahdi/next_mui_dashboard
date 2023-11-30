d17469d631c2a7b1bc9941356d949846daa1ce8a
abdulsalamalmahdi <getmeips@gmail.com>
2023-11-30 17:14:55 +0100

    fix: add next-images and next-compose-plugins to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,33 @@
 const withImages = require('next-images')
-const withPlugins = require('next-compose-plugins');
+// const withPlugins = require('next-compose-plugins');
 const isDev = process.env.NODE_ENV !== "production";
 
-module.exports = withPlugins([
+// module.exports = withPlugins([
   
-  [withImages, {
-    assetPrefix: isDev ? "": "/next_mui_dashboard/"
-}],
-  ,{
+//   [
+//     withImages, 
+//     {
+//     assetPrefix: isDev ? "": "/next_mui_dashboard/"
+// }
+// ],
+//   ,{
+//   reactStrictMode: true,
+//   output:"export",
+//   basePath: process.env.NODE_ENV === 'production' ? "/next_mui_dashboard" : undefined,
+
+//   assetPrefix: process.env.NODE_ENV === 'production' ? '/next_mui_dashboard/':null, // assetPrefix requires the trailing slash
+//   images:{
+//     unoptimized: true,
+//   }, 
+
+//   // fastRefresh: true,
+//   // productionBrowserSourceMaps: false, // Disable source maps in development
+//   // optimizeFonts: false, // Disable font optimization
+//   // minify: false, // Disable minification
+// }]
+// )
+
+const nextConfig = {
   reactStrictMode: true,
   output:"export",
   basePath: process.env.NODE_ENV === 'production' ? "/next_mui_dashboard" : undefined,
@@ -16,10 +36,9 @@ module.exports = withPlugins([
   images:{
     unoptimized: true,
   }, 
+}
 
-  // fastRefresh: true,
-  // productionBrowserSourceMaps: false, // Disable source maps in development
-  // optimizeFonts: false, // Disable font optimization
-  // minify: false, // Disable minification
-}]
-)
+module.exports =(_phase, {defaultConfig})=>{
+  const plugins = [withImages]
+  return plugins.reduce((acc,plugin)=> plugin(acc), {...defaultConfig, ...nextConfig })
+}
