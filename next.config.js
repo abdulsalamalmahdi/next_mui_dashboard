59a3ea9f72ac2a1e93efe5ff5cf1bfe5b67120e3
abdulsalamalmahdi <getmeips@gmail.com>
2023-11-30 12:26:31 +0100

    fix: assetspath

--- next.config.js
+++ next.config.js
@@ -2,10 +2,12 @@ module.exports = {
   reactStrictMode: true,
   output:"export",
   basePath: process.env.NODE_ENV === 'production' ? "" : undefined,
+
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://abdulsalamalmahdi.github.io/next_mui_dashboard/':null, // assetPrefix requires the trailing slash
   images:{
     unoptimized: true,
   }, 
-  
+
   // fastRefresh: true,
   // productionBrowserSourceMaps: false, // Disable source maps in development
   // optimizeFonts: false, // Disable font optimization
