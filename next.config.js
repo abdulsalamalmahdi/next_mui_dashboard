c6ad0057a38f02a9c0eae683097978ffc4959fda
abdulsalamalmahdi <getmeips@gmail.com>
2023-11-30 10:09:59 +0100

    add next config settings  for github pages deployment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   reactStrictMode: true,
-  fastRefresh: true,
-  productionBrowserSourceMaps: false, // Disable source maps in development
-  optimizeFonts: false, // Disable font optimization
-  minify: false, // Disable minification
+  output:"export",
+  basePath: process.env.NODE_ENV === 'production' ? "" : undefined,
+  images:{
+    unoptimized: true,
+  }, 
+  // fastRefresh: true,
+  // productionBrowserSourceMaps: false, // Disable source maps in development
+  // optimizeFonts: false, // Disable font optimization
+  // minify: false, // Disable minification
 };
